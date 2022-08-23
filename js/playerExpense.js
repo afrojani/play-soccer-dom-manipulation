7b15c390b52750208bca7c7b856eecc4adfac7f6
afrojani <sultana.ani2019@gmail.com>
2022-08-22 22:37:53 -0500

    budget js done

--- js/playerExpense.js
+++ js/playerExpense.js
@@ -2,7 +2,7 @@ function getInputFieldValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
     const inputFieldValueString = inputField.value;
     const inputFieldValue = parseFloat(inputFieldValueString);
-    inputField.value = '';
+    // inputField.value = '';
     return inputFieldValue;
 }
 function getTextElementValueById(elementId) {
@@ -21,4 +21,12 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const perPlayerCost = getInputFieldValueById('player-cost');
     const totalPlayerCost = perPlayerCost * 5;
     setTextElementValueBYId('totalplayer-cost', totalPlayerCost);
+})
+
+document.getElementById('total-calculation').addEventListener('click', function () {
+    const totalPlayerExpenses = getTextElementValueById('totalplayer-cost');
+    const managerCost = getInputFieldValueById('manager-cost');
+    const coachCost = getInputFieldValueById('coach-cost');
+    const overallCost = totalPlayerExpenses + managerCost + coachCost;
+    setTextElementValueBYId('overall-total', overallCost);
 })
